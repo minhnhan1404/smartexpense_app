ebac3293d196a80c3506f87aa443495ed2782ecf
minhnhan1404 <minhnhan1442003@gmail.com>
2026-08-13 16:38:35 +0700

    Update API base URL to Cloudflare tunnel

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -2,18 +2,16 @@ import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ApiConfig {
-  // Thay đổi IP này thành IP Wi-Fi của máy tính bạn hoặc tên miền khi lên Store
-  static const String serverIp = '192.168.2.84'; // IP Wi-Fi hiện tại của bạn
+  static const String serverIp = 'seat-buildings-scanners-here.trycloudflare.com';
   
   static String get baseUrl {
     if (kIsWeb) return 'http://127.0.0.1/SmartExpense/public/api';
     try {
-      if (Platform.isAndroid) return 'http://$serverIp/SmartExpense/public/api';
+      if (Platform.isAndroid) return 'https://$serverIp/SmartExpense/public/api';
     } catch (e) {}
     return 'http://127.0.0.1/SmartExpense/public/api';
   }
 
-  // Tiện ích nối đường dẫn
   static String getUrl(String endpoint) {
     return '$baseUrl/$endpoint';
   }
